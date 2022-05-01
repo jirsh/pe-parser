e24c14b4e02769338a2bba4eb91b76f3739689ad
jirsh <jirsh@users.noreply.github.com>
2022-05-01 11:00:11 +0200

    Added the validation module

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pe-parser
  PE Parser for Node.JS applications
+
+### Validation example
+```js
+// The Validate promise throws if the PE file is invalid
+try {
+    const { architecture } = await Validate({file}); // returns either 'x64' or 'x86'
+    
+    console.log(architecture);
+} catch (e) {
+    console.error(e);
+}
+```
---
+++ src/validate.ts
@@ -0,0 +1,31 @@
+type ValidateParams = {
+    file: Buffer;
+}
+
+type ValidateReturn = {
+    architecture: 'x64' | 'x86';
+}
+
+const IMAGE_FILE_MACHINE_I386 = 0x014c;
+const IMAGE_FILE_MACHINE_IA64 = 0x0200;
+const IMAGE_FILE_MACHINE_AMD64 = 0x8664;
+
+export function Validate({file}: ValidateParams): Promise<ValidateReturn> {
+    return new Promise((resolve, reject) => {
+        if (file.length < 97) // http://www.phreedom.org/research/tinype
+            return reject('File too small');
+        
+        if (0x5A4D !== file.readInt16LE(0)) // Check if the magic value in the DOS header is 'MZ'
+            return reject('Invalid DOS header');
+
+        const e_lfanew = file.readInt32LE(60); // New header pointer
+        if (0x4550 !== file.readInt32LE(e_lfanew)) // Check if the new header signature is 'PE\0\0'
+            return reject('Invalid PE header');
+
+        const machine = file.readUint16LE(e_lfanew + 4);
+        if (machine !== IMAGE_FILE_MACHINE_I386 && machine !== IMAGE_FILE_MACHINE_AMD64) // Check if this is a header we're familiar with
+            return reject('PE file not parsable');
+
+        resolve({architecture: machine === IMAGE_FILE_MACHINE_I386 ? 'x86' : 'x64'});
+    });
+}
